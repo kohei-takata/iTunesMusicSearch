d1d7148af1482fb06b9dab65a06d35ed740d4acd
Kohei TAKATA <kt.koheitakata@gmail.com>
2016-04-15 22:13:21 +0900

    Before Logger

---
+++ iTunesMusicSearch/iTunesMusicSearch.m
@@ -0,0 +1,9 @@
+//
+//  iTunesMusicSearch.m
+//  iTunesMusicSearch
+//
+//  Created by 高田 孝平 on 2016/04/15.
+//  Copyright © 2016年 Kohei TAKATA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
