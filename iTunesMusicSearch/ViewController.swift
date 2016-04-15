c58804482c043d0e5ddc6ab2c6dcedb899f8fc57
Kohei TAKATA <kt.koheitakata@gmail.com>
2016-04-15 22:03:50 +0900

    Initial Commit

---
+++ iTunesMusicSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTunesMusicSearch
+//
+//  Created by 高田 孝平 on 2016/04/15.
+//  Copyright © 2016年 Kohei TAKATA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
